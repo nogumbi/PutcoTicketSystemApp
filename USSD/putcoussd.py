1c8802d4b7c522315be8260f8f32b39f8c8b2aee
nogumbi <nogumbi@student.wethinkcode.co.za>
2021-06-02 22:01:55 +0200

    phone and pin functions added

--- USSD/putcoussd.py
+++ USSD/putcoussd.py
@@ -11,15 +11,65 @@ def create_num():
     Inputs the 9 digit cellphone number 
     file after registeration for later use.
     """
-    phone_num = input("Please enter your phone number (+27 __ ___ ____):")
-    while len(phone_num) != 9 or not phone_num.isdigit():
-        print("Invalid Entry")
-        phone_num = input("Please enter your phone number (+27 __ ___ ____)")
-
-    return phone_num
+    phone_num = input("Please enter your phone number (+27 __ ___ ____): ")
+    if len(phone_num) != 9 or not phone_num.isdigit():
+        print("Invalid Phone Number.\n")
+        return create_num()
+    else:
+        print(f'+27 {phone_num}')
+        return (f'+27 {phone_num}')
 
 def create_pin():
+    """Creates pin for the first time
+    that will be connected with the phone
+    number
+
+    Returns:
+        int: pin that will be stored in the data.json as a 
+        dictionary
+    """
+    pin = input("Please enter a 5-digit pin: ")
+    if len(pin) != 5 or not pin.isdigit():
+        print("Invalid pin.\n")
+        return create_pin()
+    else:
+        return pin
+
+def options():
+    """
+    Options available
+    """
     pass
+
+def registration():
+    """
+    Process of storing new users details to the data.json
+    """
+    print("Registering...")
+    print("Successfully registered...")
+    pass
+
+def login():
+    """
+    Accessing the json file to match the number
+    entered to the pin
+    """
+    pass
+
+def linkcard():
+    """
+    Linking bank card to refill
+    """
+    print("Linking card...")
+    pass
+
+def refill():
+    """
+    Refill to the number at the back of the bus tag
+    """
+    print("Bus tag number xxx-xxx-xxxx refill. ")
+    pass
+
 
 if __name__ == '__main__':
     print("Welcome to the Putco Electronic Refill System.\n \n")
--- USSD/test_putco.py
+++ USSD/test_putco.py
@@ -4,15 +4,13 @@ import putcoussd
 class Test(unittest.TestCase):
     # Tests if the bus number has 10 digits
     def test_numberofdigits(self):
-        for i in range(100):
-            code = putcoussd.create_num()
-            self.assertEqual(len(code), 10)
+        code = putcoussd.create_num()
+        self.assertEqual(len(code), 9)
     
     # Tests the pin number
     def test_pin(self):
-       for i in range(100):
-            code = putcoussd.create_pin()
-            self.assertEqual(len(code), 5)
+        code = putcoussd.create_pin()
+        self.assertEqual(len(code), 5)
 
 if __name__ == '__main__':
     unittest.main()
