4f0b2125a1e2ff7f91514d0bdea41c79318b34be
nogumbi <nogumbi@student.wethinkcode.co.za>
2021-06-13 13:58:29 +0200

    maven dependencies added

---
+++ src/test/java/PutcoTest.java
@@ -0,0 +1,17 @@
+//import putcoTicketingSystem.Putco;
+//
+//import org.junit.jupiter.api.Test;
+//
+//public class PutcoTest {
+//    @Test
+//    public void testFile()
+//    {}
+//
+//    @Test
+//    public void testLogin()
+//    {}
+//
+//    @Test
+//    public void removeElement()
+//    {}
+//}
